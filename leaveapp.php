bb97244efd415f5b0dfa6fc238764642425aa51e
sagor1155 <sagor.eee.cuet@gmail.com>
2020-04-08 19:32:51 +0600

    form data inserted to database

--- leaveapp.php
+++ leaveapp.php
@@ -8,6 +8,7 @@
 <body>
     <?php
         echo htmlspecialchars($_SERVER["PHP_SELF"]) . "</br></br>";
+
         function handleFormPostRequest(){
             $employee_name  = verifyPostTextData("employee_name");
             $designation    = verifyPostTextData("employee_designation");
@@ -36,10 +37,18 @@
             echo "dateinput_from: "     . $dateinput_from       . '</br>';
             echo "dateinput_to: "       . $dateinput_to         . '</br>';
             echo "employee_contact: "   . $employee_contact     . '</br>';
+
+            $dbobject = new Database("mydb");
+            $dbobject->connect();
+            // $dbobject->createTable('leaveApp');
+            $dbobject->insertData('leaveApp', $employee_name, $designation, $department, "SIL", $dateinput_from, $dateinput_to, 
+                                $leave_purpose, $leave_address, $employee_contact, $duties_carried_by, $supervisor_select, $hod_select);
+            $dbobject->disconnect();
         }
 
         if ($_SERVER["REQUEST_METHOD"] == "POST") {
             handleFormPostRequest();
+
         }
 
         function test_input($data) {
@@ -61,6 +70,99 @@
             }
             return $temporay_var;
         }
+
+
+        class Database{
+            public $db_connect;
+            public $dbname;
+            const SERVERNAME = "localhost";
+            const USERNAME   = "root";
+            const PASSWORD   = "";
+
+            public function __construct($dbName){
+                $this->dbname = $dbName;
+            }
+
+            public function connect(){
+                // create db connection
+                $this->db_connect = new mysqli(self::SERVERNAME, self::USERNAME, self::PASSWORD, $this->dbname);
+
+                // check connection establishment 
+                if($this->db_connect->connect_error){
+                    die("db connection failed! " . $this->db_connect->connect_error);
+                }else{
+                    echo "db connection successfull </br>";
+                }
+            }
+
+            public function createTable($tableName){
+                $sql = "CREATE TABLE $tableName (
+                        id                  INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+                        employee_name       VARCHAR(40)     NOT NULL,
+                        designation         VARCHAR(30)     NOT NULL,
+                        department          VARCHAR(40)     NOT NULL,
+                        company_div         VARCHAR(10)     NOT NULL,
+                        leave_from          DATE,
+                        leave_to            DATE,
+                        leave_purpose       VARCHAR(50),
+                        leave_address       VARCHAR(50),
+                        contact             VARCHAR(20),
+                        duties_carried_by   VARCHAR(40),
+                        supervisor          VARCHAR(40),
+                        hod                 VARCHAR(40)
+                        )";
+
+                if ($this->db_connect->query($sql) === TRUE) {
+                    echo "Table $tableName created successfully";
+                } else {
+                    echo "Error creating table: " . $this->db_connect->error;
+                }
+            }
+
+            public function insertData($tableName, $name, $designation, $dept, $div, $from, $to, $purpose, $addr, $contact, $duty_carry, $sup, $hod){
+                $sql = "INSERT INTO $tableName (employee_name, designation, department, company_div, leave_from, leave_to, leave_purpose, leave_address, contact, duties_carried_by, supervisor, hod) 
+                        VALUES ('$name', '$designation', '$dept', '$div', '$from', '$to', '$purpose', '$addr', '$contact', '$duty_carry', '$sup', '$hod')";
+
+                if ($this->db_connect->query($sql) === TRUE) {
+                    $last_id = $this->db_connect->insert_id;
+                    echo "Data inserted successfully to $tableName table, at id=$last_id </br>";
+                } else {
+                    echo "Error inserting data: " . $this->db_connect->error;
+                }
+            }
+
+            public function updateData($tableName, $dataField, $data, $id){
+                $sql = "UPDATE $tableName SET $dataField='$data' WHERE id=$id";
+                if ($this->db_connect->query($sql) === TRUE) {
+                    echo "Record updated successfully";
+                } else {
+                    echo "Error updating record: " . $this->db_connect->error;
+                }
+            }
+
+            public function deleteData($tableName, $id){
+                $sql = "DELETE FROM $tableName WHERE id=$id";
+                if ($this->db_connect->query($sql) === TRUE) {
+                    echo "Record deleted successfully </br>";
+                } else {
+                    echo "Error deleting record: " . $this->db_connect->error;
+                }
+            }
+
+            // public function createDatabase($db){ 
+            //     $sql = "CREATE DATABASE $db";
+            //     if($this->db_connect->query($sql)){
+            //         echo "database created successfully </br>";
+            //     }else{
+            //         echo "error creating database. " . $this->db_connect->error . "</br>";
+            //     }      
+            // }
+
+            public function disconnect(){
+                $this->db_connect->close();
+                echo "db disconnected </br>";
+            }
+        }  
 
     ?>    
 </body>
